51fafff3aebb24e1e4d0f25e900dc3a2d0c630b9
ramishalah <romioo1010@gmail.com>
2018-12-04 18:51:06 +0300

    pushing the first commit to the github

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,10 @@
 package com.company;
 
+import javax.swing.plaf.synth.SynthTextAreaUI;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("hello world");
     }
 }
